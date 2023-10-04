576dc8435ee0a97a61420acd7c31f95cd4451636
7433jun <7433jun@naver.com>
2023-10-04 21:43:56 +0900

    (2023.10.4) 오브젝트 풀링

---
+++ Game/Assets/Object Pool/Scripts/ObjectPoolManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    static public ObjectPoolManager instance;
+
+    [SerializeField] GameObject bulletPrefab;
+
+    Queue<GameObject> bulletQueue = new Queue<GameObject>();
+
+    [SerializeField] int bulletCount = 5;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < bulletCount; i++)
+        {
+            bulletQueue.Enqueue(Instantiate(bulletPrefab));
+        }
+
+        foreach(var e in bulletQueue)
+        {
+            e.SetActive(false);
+        }
+    }
+
+    public void InsertQueue(GameObject bullet)
+    {
+        bulletQueue.Enqueue(bullet);
+        bullet.SetActive(false);
+    }
+
+    public GameObject GetQueue()
+    {
+        GameObject bullet = bulletQueue.Dequeue();
+        bullet.SetActive(true);
+
+        return bullet;
+    }
+}
